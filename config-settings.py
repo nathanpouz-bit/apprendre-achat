d1a48ca43318c7fda8cc65f6c6eef4cb0d5d3a00
nathanpouz-bit <nathanael.pouzin@gmail.com>
2026-06-21 11:42:33 +0200

    Add required columns to config-settings.py

---
+++ config-settings.py
@@ -0,0 +1,20 @@
+# config/settings.py
+
+REQUIRED_COLUMNS = [
+    "Segment",
+    "Country",
+    "Product",
+    "Discount Band",
+    "Units Sold",
+    "Manufacturing Price",
+    "Sale Price",
+    "Gross Sales",
+    "Discounts",
+    "Sales",
+    "COGS",
+    "Profit",
+    "Date",
+    "Month Number",
+    "Month Name",
+    "Year"
+]
